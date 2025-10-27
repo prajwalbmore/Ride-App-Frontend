c57971108ee5ab5b173552f21754655ebde0f6fd
prajwalmoreCM <prajwal.m@compumatrixtechnologies.com>
2025-10-27 22:12:18 +0530

    nice

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // where final files go
+    chunkSizeWarningLimit: 1000, // optional: remove large chunk warnings
+  },
 })
